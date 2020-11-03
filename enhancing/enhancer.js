3766cc0ab96b903ea5a7b0f6c49431592713b3e8
unknown <whitta14@gmail.com>
2020-11-03 18:16:01 -0500

    completed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -4,19 +4,45 @@ module.exports = {
   repair,
   get,
 };
-
+ 
 function success(item) {
+  
+  if (item.enhancement >= 20){
+    item.enhancement = 20
+    return {...item}
+  }
+
+
+  item.enhancement++
+  
   return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15){
+    item.durability = item.durability -5;
+  }
+
+  if (item.enhancement >= 15){
+    item.durability = item.durability -10;
+  }
+
+  if (item.enhancement > 16){
+    item.enhancement--;
+  }
+
+  return { ...item }
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
 function get(item) {
+  if (item.enhancement > 0){
+    item.name = `[+${item.enhancement}] ${item.name}`
+  }
+  
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,85 @@
+const { test, expect } = require('@jest/globals');
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+
+test("repair", () => {
+    expect(enhancer.repair({
+        name: "Ephemeral Tranquility",
+        durability: 40,
+        enhancement: 2,
+    })).toEqual({
+        name: "Ephemeral Tranquility",
+        durability: 100,
+        enhancement: 2,
+    })
+}) 
+
+test("success",() => {
+    expect(enhancer.success({
+        name: "Ephemeral Tranquility",
+        durability: 40,
+        enhancement: 2,
+    })).toEqual({
+        name: "Ephemeral Tranquility",
+        durability: 40,
+        enhancement: 3,
+    })
+
+    expect(enhancer.success({
+        name: "Ephemeral Tranquility",
+        durability: 40,
+        enhancement: 20,
+    })).toEqual({
+        name: "Ephemeral Tranquility",
+        durability: 40,
+        enhancement: 20,
+    })
+})
+
+test("fail", () => {
+    expect(enhancer.fail({
+        name: "Ephemeral Tranquility",
+        durability: 40,
+        enhancement: 2,
+    })).toEqual({
+        name: "Ephemeral Tranquility",
+        durability: 35,
+        enhancement: 2,
+    })
+
+    expect(enhancer.fail({
+        name: "Ephemeral Tranquility",
+        durability: 40,
+        enhancement: 15,
+    })).toEqual({
+        name: "Ephemeral Tranquility",
+        durability: 30,
+        enhancement: 15,
+    })
+
+    expect(enhancer.fail({
+        name: "Ephemeral Tranquility",
+        durability: 30,
+        enhancement: 17,
+    })).toEqual({
+        name: "Ephemeral Tranquility",
+        durability: 20,
+        enhancement: 16,
+    })
+})
+
+
+test("get", () => {
+    expect(enhancer.get({
+        name: "Ephemeral Tranquility",
+        durability: 30,
+        enhancement: 7,
+    })).toEqual({
+        name: "[+7] Ephemeral Tranquility",
+        durability: 30,
+        enhancement: 7,
+    })
+})
+
